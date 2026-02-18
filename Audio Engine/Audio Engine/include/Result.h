6da096dbc5bd6dfb9b987f06e1a5f279ccb14978
Arvid Wahlgren <salliban03@gmail.com>
2026-02-18 16:29:09 +0100

    Better results

--- Audio Engine/Audio Engine/include/Result.h
+++ Audio Engine/Audio Engine/include/Result.h
@@ -1,22 +1,35 @@
 #pragma once
 
+#define PLACEHODLER_NAME_OK(res) (static_cast<int>(res) >= 0)
+#define PLACEHOLDER_NAME_ERROR(res) (static_cast<int>(res) < 0)
+
 namespace placeholder_name {
 
 	enum class Result : int {
 		Ok = 0,
-		Error = -1,
 
-		// MiniAudio
-		DeviceFailed = -3,
+		// Generic Errors [-1, -99]
+		Error = -1,
+		NotInitialized = -2,
+		AlreadyInitialized = -3,
+
+		// Backed Errors [-100, -199]
+		DeviceFailed = -100,
 	};
 
-	inline const char* GetErrorString(Result result) {
+	constexpr const char* GetErrorString(Result result) {
+		// Update this every time you add a new result
 		switch (result) {
 			case Result::Ok: return "Ok: No error.";
-			case Result::Error: return "Generic error";
-			case Result::DeviceFailed: return "Playback device failed";
-			default: return "Unknown error";
-			// Keep updating this
+
+			case Result::Error: return "Generic error.";
+			case Result::NotInitialized: return "Engine is not initialized.";
+			case Result::AlreadyInitialized: return "Engine is already initialized.";
+
+			case Result::DeviceFailed: return "Playback device failed.";
+			default: return "Unknown error.";
+		}
+	}
 		}
 	}
 }
