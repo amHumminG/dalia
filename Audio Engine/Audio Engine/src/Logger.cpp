215556254f42c58c121265379473261258e2ffb4
Arvid Wahlgren <salliban03@gmail.com>
2026-02-19 20:57:01 +0100

    Corrected getlevelstring

--- Audio Engine/Audio Engine/src/Logger.cpp
+++ Audio Engine/Audio Engine/src/Logger.cpp
@@ -48,6 +48,7 @@ namespace placeholder_name {
 	}
 	const char* Logger::GetLevelString(LogLevel level) {
 		switch (level) {
+		case LogLevel::Debug:		return "DEBUG";
 		case LogLevel::Info:		return "INFO";
 		case LogLevel::Warning:		return "WARNING";
 		case LogLevel::Error:		return "ERROR";
