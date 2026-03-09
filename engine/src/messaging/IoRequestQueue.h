28f17cd055e21de852a581e648a22e6a951e5a53
Arvid Wahlgren <salliban03@gmail.com>
2026-03-10 00:21:47 +0100

    I/O logic for preparing, releasing and refilling streaming buffers from .ogg file

--- engine/src/messaging/IoRequestQueue.h
+++ engine/src/messaging/IoRequestQueue.h
@@ -3,14 +3,17 @@
 
 namespace dalia {
 
+    static constexpr size_t MAX_IO_PATH_SIZE = 256;
+
     struct StreamingContext;
     struct Soundbank;
 
     struct IoRequest {
         enum class Type {
             None,
-            RefillStreamBuffer,
+            PrepareStream,
             ReleaseStream,
+            RefillStreamBuffer,
             LoadSoundbank
         };
         Type type = Type::None;
@@ -20,20 +23,20 @@ namespace dalia {
                 uint32_t poolIndex;
                 uint32_t  generation;
                 uint32_t bufferIndex;
+                char path[MAX_IO_PATH_SIZE];
             } stream;
 
             struct {
-                Soundbank* targetBank; // TODO: This should not be pointer (Change to an index in a soundbank table)
-                uint32_t assetID; // Maybe this should be a filepath? (if not we need a lookup table)
+                uint32_t bankId; // TODO: This should not be pointer (Change to an index in a soundbank table)
+                uint32_t assetId; // Maybe this should be a filepath? (if not we need a lookup table)
             } bank;
         } data = {};
 
-        static IoRequest RefillStreamBuffer(uint32_t poolIndex, uint32_t generation, uint32_t bufferIndex) {
+        static IoRequest PrepareStream(uint32_t poolIndex, char* path) {
             IoRequest req;
-            req.type = Type::RefillStreamBuffer;
+            req.type = Type::PrepareStream;
             req.data.stream.poolIndex = poolIndex;
-            req.data.stream.generation = generation;
-            req.data.stream.bufferIndex = bufferIndex;
+            req.data.stream.path = path;
             return req;
         }
 
@@ -44,10 +47,19 @@ namespace dalia {
             return req;
         }
 
-        static IoRequest LoadBank(Soundbank* targetBank, const uint32_t assetID) {
+        static IoRequest RefillStreamBuffer(uint32_t poolIndex, uint32_t generation, uint32_t bufferIndex) {
+            IoRequest req;
+            req.type = Type::RefillStreamBuffer;
+            req.data.stream.poolIndex = poolIndex;
+            req.data.stream.generation = generation;
+            req.data.stream.bufferIndex = bufferIndex;
+            return req;
+        }
+
+        static IoRequest LoadBank(uint32_t bankId, const uint32_t assetID) {
             IoRequest req;
             req.type = Type::LoadSoundbank;
-            req.data.bank.targetBank = targetBank;
+            req.data.bank.targetBank = bankId;
             req.data.bank.assetID = assetID;
             return req;
         }
--- engine/src/systems/IoSystem.cpp
+++ engine/src/systems/IoSystem.cpp
@@ -1,8 +1,12 @@
 #include "systems/IoSystem.h"
 
+#include "core/Logger.h"
+
 #include "core/SPSCRingBuffer.h"
 #include "messaging/IoRequestQueue.h"
 #include "mixer/StreamingContext.h"
+
+#include "stb_vorbis.c"
 
 #include <chrono>
 
@@ -42,30 +46,85 @@ namespace dalia {
 
                 // Handle requests
                 switch (req.type) {
-                case IoRequest::Type::RefillStreamBuffer: {
-                    StreamingContext& stream = m_streamPool[req.data.stream.poolIndex];
+                    case IoRequest::Type::PrepareStream: {
+                        StreamingContext& stream = m_streamPool[req.data.stream.poolIndex];
 
-                    // Is the stream still valid?
-                    if (stream.generation.load(std::memory_order_relaxed) != req.data.stream.generation) {
+                        if (stream.decoder) {
+                            stb_vorbis_close(stream.decoder);
+                            stream.decoder = nullptr;
+                        }
+
+                        int error;
+                        stream.decoder = stb_vorbis_open_filename(req.data.stream.path, &error, nullptr);
+
+                        if (stream.decoder) {
+                            stb_vorbis_info info = stb_vorbis_get_info(stream.decoder);
+                            stream.channels = info.channels;
+                            stream.sampleRate = info.sample_rate;
+                        }
+                        else {
+                            // TODO: Invalid stream, maybe we set stream state to something like invalid so that audio thread knows its fucked? Or do we just release it here?
+                            Logger::Log(LogLevel::Error, "IoSystem", "Failed to open stream for: %s (error: %d)", req.data.stream.path, error);
+                        }
+
                         break;
                     }
+                    case IoRequest::Type::ReleaseStream: {
+                        StreamingContext& stream = m_streamPool[req.data.stream.poolIndex];
+                        if (stream.decoder) {
+                            stb_vorbis_close(stream.decoder);
+                            stream.decoder = nullptr;
+                        }
 
-                    // TODO: Read data into buffer from soundbank
-                    // Remember that if read reaches EOF, it should mark it in the buffer and keep reading
-                    // from the start of the file to fill the entire buffer.
-                }
-                case IoRequest::Type::ReleaseStream: {
-                    StreamingContext& stream = m_streamPool[req.data.stream.poolIndex];
-                    // TODO: Release decoder here (if StreamingContext has one)
-                    m_freeStreams->Push(req.data.stream.poolIndex);
-                    break;
-                }
-                case IoRequest::Type::LoadSoundbank: {
-                    // TODO: Logic for soundbank loading
-                    break;
-                }
-                default:
-                    break;
+                        stream.Reset();
+                        stream.state = StreamState::Free;
+                        m_freeStreams->Push(req.data.stream.poolIndex);
+                        break;
+                    }
+                    case IoRequest::Type::RefillStreamBuffer: {
+                        StreamingContext& stream = m_streamPool[req.data.stream.poolIndex];
+
+                        // Is the stream still valid?
+                        if (stream.generation.load(std::memory_order_relaxed) != req.data.stream.generation) {
+                            break;
+                        }
+
+                        if (!stream.decoder) {
+                            // TODO: We need to signal to the audio thread that decoder is invalid here somehow!
+                            Logger::Log(LogLevel::Error, "Stream buffer refill", "Invalid decoder");
+                            break;
+                        }
+
+                        float* bufferPtr = stream.buffers[req.data.stream.bufferIndex];
+                        int samplesNeeded = DOUBLE_BUFFER_FRAMES * stream.channels;
+                        int samplesWritten = 0;
+
+                        while (samplesWritten < samplesNeeded) {
+                            int samplesRead = stb_vorbis_get_samples_float_interleaved(
+                                stream.decoder,
+                                stream.channels,
+                                bufferPtr + samplesWritten,
+                                samplesNeeded - samplesWritten
+                            );
+
+                            if (samplesRead == 0) {
+                                // Hit EOF
+                                stream.eofIndex[req.data.stream.bufferIndex] = samplesWritten;
+                                stb_vorbis_seek_start(stream.decoder);
+                                // We continue to fill buffer from the start of the file
+                            }
+
+                            samplesWritten += samplesRead;
+                        }
+
+                        stream.bufferReady[req.data.stream.bufferIndex].store(true, std::memory_order_release);
+                    }
+                    case IoRequest::Type::LoadSoundbank: {
+                        // TODO: Logic for soundbank loading
+                        break;
+                    }
+                    default:
+                        break;
                 }
             }
 
