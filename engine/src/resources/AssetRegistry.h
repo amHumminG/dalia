50c922dc3e6c481a066b1d9237471c6911d92a84
Arvid Wahlgren <salliban03@gmail.com>
2026-03-13 21:20:00 +0100

    Asset registry

---
+++ engine/src/resources/AssetRegistry.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "dalia/audio/ResourceHandle.h"
+#include "core/HandlePool.h"
+
+// Resource types
+#include "resources/ResidentSound.h"
+// #include "resources/StreamDescriptor"
+// #include "resources/EventDescriptor"
+// #include "resourcs/BankRepresentation"
+
+#include <unordered_map>
+#include <mutex>
+
+namespace dalia {
+
+    class AssetRegistry {
+    public:
+
+
+    private:
+        
+    };
+}
