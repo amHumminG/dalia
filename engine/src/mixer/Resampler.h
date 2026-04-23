3b8ad9e786339296580d25536fe669996a9b3363
Arvid Wahlgren <salliban03@gmail.com>
2026-04-23 23:54:16 +0200

    Resampling algorithm (hermite interpolation)

---
+++ engine/src/mixer/Resampler.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "core/Constants.h"
+
+#include <cstdint>
+
+namespace dalia {
+
+	// struct ResamplerState {
+	// 	float fractionalPhase = 0.0f;
+	// 	float history[3 * CHANNELS_MAX] = {0.0f};
+	// };
+
+	struct ResamplerState {
+		float fractionalPhase = 0.0f;
+		float history[CHANNELS_MAX] = {0.0f};
+	};
+
+	void ProcessResampler(
+		ResamplerState& state,
+		const float* sourceData,
+		uint32_t maxSourceFrames,
+		uint32_t channels,
+		float* outBuffer,
+		uint32_t maxOutputFrames,
+		float phaseIncrement,
+		uint32_t& outFramesGenerated,
+		uint32_t& sourceFramesConsumed
+	);
+}
